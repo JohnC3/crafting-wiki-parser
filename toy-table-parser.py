45a5604a16d0be0e307a63a02285512346c72aeb
John Clements <jclements@fb.com>
2018-03-23 15:29:51 -0700

    fixed regexes

--- toy-table-parser.py
+++ toy-table-parser.py
@@ -5,20 +5,43 @@ from __future__ import unicode_literals
 
 
 import re
+import io
+
+
+class vertex(object):
+
+    def __init__(self, value):
+
+        self.name = name
+        self.children = []
+
+    def add_child(self, child):
+
+        assert isinstance(self, child), 'child is not a vertex!'
+
+        child.root = self
+        self.children.append(child)
+
+    def
+
 
 
 def extract_tables(fp):
     "Extract the text between table tags"
-    with open(fp, 'rb') as f:
+    with io.open(fp, 'r', encoding='utf8') as f:
 
         text = f.read()
-        print(text[:44])
-        text = text.decode()
+
+        text = text.encode('utf8', 'ignore')
     tables = re.findall(r'<table(.*)</table>', text, re.DOTALL)
     # print('tables', tables)
     print(type(tables))
     for i in tables:
         print(i)
+
+
+def extract_rows(table_text):
+
 
 
 
